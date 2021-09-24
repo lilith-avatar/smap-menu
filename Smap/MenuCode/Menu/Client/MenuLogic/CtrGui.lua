3e4261aae7256b54d06ff74aadc3b33b3d775ea0
ropztao <ropztao@lilith.com>
2021-09-24 11:55:35 +0800

    switch pnl

--- Smap/MenuCode/Menu/Client/MenuLogic/CtrGui.lua
+++ Smap/MenuCode/Menu/Client/MenuLogic/CtrGui.lua
@@ -3,15 +3,37 @@
 ---@author ropztao
 local  CtrGui,this = ModuleUtil.New('CtrGui', ClientBase)
 
-local norSize = Vector2(0.25,0.75)
+local norSize, isOpen, isChosen = Vector2(0.25,0.75), false, false
+
 local function UiAssignment(k,v)
     for i,j in pairs(v) do
         if i == 'Texture' then
             CtrGui[k][i] = ResourceManager.GetTexture(j)
-        elseif i == 'Texture2' then
         elseif i == 'Name' then
         else
             CtrGui[k][i] = j
+        end
+    end
+end
+
+---开关节点控制器
+---@param _bool 期望的布尔值
+---@param _tarTab 目标表格
+---@param _spNode 排除节点
+local function SwitchNodeCtr(_bool, _tarTab, _spNode)
+	for k,v in pairs(_tarTab) do
+		if v == _spNode then
+			v:SetActive(not _bool)
+		else
+			v:SetActive(_bool)
+		end
+    end
+end
+
+local function SwitchTextureCtr(_tar, _tab, _tex)
+    for k,v in pairs(_tab) do
+        if v ~= _tar then
+            v.Texture = ResourceManager.GetTexture('MenuRes/'.._tex)
         end
     end
 end
@@ -20,7 +42,6 @@ end
 function CtrGui:Init()
     self:DataInit()
     self:GuiInit()
-    self:ListenerInit()
 end
 
 ---数据变量初始化
@@ -34,28 +55,34 @@ function CtrGui:GuiInit()
     self.root = world:CreateObject('UiScreenUiObject', 'MenuGui', world.Local)
 
     ---menu开关
-    self.BtnSwitch = world:CreateObject('UiImageObject', 'BtnSwitch', self.root)
-    for i = 4,1,-1 do
-        self['point'..1] = world:CreateObject('UiFigureObject', 'point'..1, self.BtnSwitch)
-    end
-    self.BtnSwitch.Color = self.rootCfg.BtnBase.Color
+    self.BtnSwitch = world:CreateObject('UiButtonObject', 'BtnSwitch', self.root)
+    -- self.BtnSwitch.Color = self.rootCfg.BtnBase.Color
     self.BtnSwitch.AnchorsY = Vector2(self.rootCfg.BtnBase.AnchorsY.y, self.rootCfg.ImgBase.AnchorsY.y)
     local fixedXmin = self.root.Size.y * (self.BtnSwitch.AnchorsY.y - self.BtnSwitch.AnchorsY.x) / self.root.Size.x
     self.BtnSwitch.AnchorsX = Vector2(fixedXmin ,self.rootCfg.BtnBase.AnchorsX.y)
+    --todo 等编辑器bug修好再接着做这部分逻辑
+    for i = 4,1,-1 do
+        self['point'..i] = world:CreateObject('UiFigureObject', 'point'..1, self.BtnSwitch)
+    end
 
-
+    ---信息底板
     self.ImgBase = world:CreateObject('UiImageObject', 'ImgBase', self.root)
+    ---按钮底板
     self.BtnBase = world:CreateObject('UiFigureObject', 'BtnBase', self.root)
     self.ImgBase:SetActive(false)
     self.BtnBase:SetActive(false)
+
+    ---ui赋值
     for m,n in pairs(self.rootCfg) do
         UiAssignment(m,n)
     end
 
+    ---按钮生成和图标设置
     for k,v in pairs(self.btnCfg) do
         self[k] = world:CreateObject('UiButtonObject', tostring(v.Name), self.BtnBase)
         UiAssignment(k,v)
         self[k..'Icon'] = world:CreateObject('UiImageObject', k..'Icon', self[k])
+        self[k..'Icon'].RaycastTarget = false
         self[k..'Icon'].AnchorsX = norSize
         self[k..'Icon'].AnchorsY = norSize
         self[k..'Icon'].Texture = ResourceManager.GetTexture('MenuRes/Icon_'..string.gsub(k, 'Btn', ''))
@@ -64,11 +91,38 @@ function CtrGui:GuiInit()
     invoke(function()
         self:SizeCorrection()
     end,0.1)
+
+    self.FunBtnTab = {}
+    for k,v in pairs(self.BtnBase:GetChildren()) do
+        if v.Name ~= 'BtnQuit' then
+            table.insert(self.FunBtnTab,v)
+        end
+    end
+
+    self:ListenerInit()
 end
 
 ---事件绑定初始化
 function CtrGui:ListenerInit()
+    self.BtnSwitch.OnClick:Connect(function() 
+        if isOpen then     
+            isOpen = false
+        else 
+            isOpen = true 
+        end
+        self.ImgBase:SetActive(isOpen)
+        self.BtnBase:SetActive(isOpen)
+    end)
 
+    for k,v in pairs(self.FunBtnTab) do
+        v.OnClick:Connect(function()
+            if tostring(v.Texture) == 'Icon_Idle' then
+                v.Texture = ResourceManager.GetTexture('MenuRes/Icon_Selected')
+                SwitchTextureCtr(v, self.FunBtnTab, 'Icon_Idle')
+            end
+        end)
+    end
+    
 end
 
 ---Update函数
