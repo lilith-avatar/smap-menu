d2a026a8047fb425cded294417ad841071f46112
Yuancheng Zhang <old.far.hot@gmail.com>
2021-12-14 11:15:16 +0800

    refactor code #6: MenuCtr.lua

--- Smap/MenuCode/Menu/Client/MenuLogic/MenuCtr.lua
+++ Smap/MenuCode/Menu/Client/MenuLogic/MenuCtr.lua
@@ -4,89 +4,30 @@
 --- @author ropztao, Yuancheng Zhang
 
 -- Local Caches
-local Event, VoiceManager, Friends, Game, localPlayer = Event, VoiceManager, Friends, Game, localPlayer
+local localPlayer = localPlayer
+local Debug, Event, VoiceManager, Friends, Game = Debug, Event, VoiceManager, Friends, Game
 local C = _G.C
 
 --* 模块
 local M, this = C.ModuleUtil.New('MenuCtr', C.Base)
 
----初始化
-function M:Init()
-    self:GetFriendsList()
-    self:InitListener()
-end
-
----玩家加入
-function M:NoticeEventHandler(_playerTab, _playerList, _changedPlayer, _isAdded)
-    self.playerTab = _playerTab
-    self.playerList = _playerList
-end
-
----屏蔽所有人语音
-function M:MuteAllEventHandler(_isMuted)
-    for _, v in pairs(self.playerList) do
-        if v ~= localPlayer then
-            VoiceManager.MuteDesignatedPlayer(v.UserId, _isMuted)
-        end
-    end
-end
-
----屏蔽指定人语音
-function M:MuteSpecificPlayerEventHandler(_specificPlayer, _isMuted)
-    VoiceManager.MuteDesignatedPlayer(_specificPlayer, _isMuted)
-end
-
----拿到好友列表
-function M:GetFriendsList()
-    local list = Friends.GetFriendInfos()
-    M.Kit.Util.Net.Fire_C('GetFriendsListEvent', localPlayer, list)
-end
-
-local tt = 0
-function M:Update(dt)
-    tt = tt + dt
-    if tt > 60 then
-        self:GetFriendsList()
-        tt = 0
-    end
-end
-
-local callback = function(msg)
-    Debug.Log(msg)
-end
-
----邀请好友来游戏房间
-local localRoomIdTab = {}
-function M:InviteFriendToGameEventHandler(targetPlayerId)
-    Friends.InvitePlayer(targetPlayerId, callback)
-    localRoomIdTab['ROOMID'] = Game.GetRoomID()
-    localRoomIdTab['PLAYER'] = localPlayer
-    Event.EmitLuaEvent(Event.Scope.APP, 'MENU_INVITE', '100000000', localRoomIdTab)
-    localRoomIdTab = {}
-end
-
----去好友的游戏房间
-local content = {}
-function M:JoinFriendGameEventHandler(_player, _tarPlayerUserId)
-    table.insert(content, 'JOIN_REQUEST')
-    Event.EmitLuaEvent(Event.Scope.APP, 'MENU_JOIN', '100000001', content)
-    content = {}
-end
-
-local callback1 = function(msg)
-end
-
----添加好友
-function M:AddFriendsEventHandler(_playerId)
-    Friends.SendFriendRequest(_playerId, callback1)
-end
-
+-- 本地变量
 local roomIdTab = {}
+local playerTab, playerList
+local localRoomIdTab = {}
+local content = {}
+local tt = 0
+
+---初始化
+function Init()
+    InitListener()
+    GetFriendsList()
+end
 
 ---邀请和加入的事件初始化监听
-function M:InitListener()
+function InitListener()
     local J_callback = function(_eventType, _eventId, _eventData)
-        for k, v in pairs(_eventData) do
+        for _, v in pairs(_eventData) do
             if v == 'JOIN_REQUEST' then
                 roomIdTab['ROOMID'] = Game.GetRoomID()
                 Event.EmitLuaEvent(Event.Scope.APP, 'MENU_JOIN_R', '100000002', roomIdTab)
@@ -110,8 +51,79 @@ function M:InitListener()
     Event.ListenLuaEvent(Event.Scope.APP, 'MENU_JOIN_R', '100000002', R_callback)
 end
 
-function M:ConfirmInviteEventHandler(_player, _roomId)
+---拿到好友列表
+function GetFriendsList()
+    local list = Friends.GetFriendInfos()
+    M.Kit.Util.Net.Fire_C('GetFriendsListEvent', localPlayer, list)
+end
+
+function Update(_, dt)
+    tt = tt + dt
+    if tt > 60 then
+        GetFriendsList()
+        tt = 0
+    end
+end
+
+---玩家加入
+function NoticeEventHandler(_, _playerTab, _playerList, _changedPlayer, _isAdded)
+    playerTab = _playerTab
+    playerList = _playerList
+end
+
+---屏蔽所有人语音
+function MuteAllEventHandler(_, _isMuted)
+    for _, v in pairs(playerList) do
+        if v ~= localPlayer then
+            VoiceManager.MuteDesignatedPlayer(v.UserId, _isMuted)
+        end
+    end
+end
+
+---屏蔽指定人语音
+function MuteSpecificPlayerEventHandler(_, _specificPlayer, _isMuted)
+    VoiceManager.MuteDesignatedPlayer(_specificPlayer, _isMuted)
+end
+
+---邀请好友来游戏房间
+function InviteFriendToGameEventHandler(_targetPlayerId)
+    local callback = function(_msg)
+        Debug.Log(_msg)
+    end
+    Friends.InvitePlayer(_targetPlayerId, callback)
+    localRoomIdTab['ROOMID'] = Game.GetRoomID()
+    localRoomIdTab['PLAYER'] = localPlayer
+    Event.EmitLuaEvent(Event.Scope.APP, 'MENU_INVITE', '100000000', localRoomIdTab)
+    localRoomIdTab = {}
+end
+
+---去好友的游戏房间
+function JoinFriendGameEventHandler(_, _player, _tarPlayerUserId)
+    table.insert(content, 'JOIN_REQUEST')
+    Event.EmitLuaEvent(Event.Scope.APP, 'MENU_JOIN', '100000001', content)
+    content = {}
+end
+
+---添加好友
+function AddFriendsEventHandler(_, _playerId)
+    local callback = function(_msg)
+        Debug.Log(_msg)
+    end
+    Friends.SendFriendRequest(_playerId, callback)
+end
+
+function ConfirmInviteEventHandler(_, _player, _roomId)
     M.Kit.Util.Net.Fire_S('TeleportPlayerToFriendGameEvent', _player, _roomId)
 end
 
+--! Public methods
+M.Init = Init
+M.Update = Update
+M.NoticeEventHandler = NoticeEventHandler
+M.MuteAllEventHandler = MuteAllEventHandler
+M.MuteSpecificPlayerEventHandler = MuteSpecificPlayerEventHandler
+M.InviteFriendToGameEventHandler = InviteFriendToGameEventHandler
+M.JoinFriendGameEventHandler = JoinFriendGameEventHandler
+M.AddFriendsEventHandler = AddFriendsEventHandler
+M.ConfirmInviteEventHandler = ConfirmInviteEventHandler
 return M
