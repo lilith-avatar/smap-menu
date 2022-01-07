79598b6a0dbae79a626aab5915d275f69cfec2f8
Yuancheng Zhang <old.far.hot@gmail.com>
2022-01-07 22:18:57 +0800

    优化人物转动#2

--- Smap/MenuCode/Outfit/Script/Gui/GuiAvatar.lua
+++ Smap/MenuCode/Outfit/Script/Gui/GuiAvatar.lua
@@ -23,10 +23,8 @@ local figTouchZone  --* 触摸区域panel节点，也用于计算fig的大小
 
 --* 本地变量
 local mousePos = nil --! TEST ONLY: 开启鼠标右键监听事件
-local rawAngle
-local rawAnglePrevious
-local interpAngle
-local maxDelta, delta
+local rawAngle, rawAnglePrevious, interpAngle = 0, 0, 0
+local maxDelta, delta = 0, 0
 
 --* 枚举常量
 -- 事件常量
